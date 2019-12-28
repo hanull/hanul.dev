ac861bb92fff63d21119a4439214887adc4d4ab3
hanull <dkfghg3@naver.com>
2019-12-28 20:19:20 +0900

    footer 이름 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+﻿import React from 'react'
 
 import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/hanull">Hanul</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
